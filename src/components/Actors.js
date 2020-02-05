5d08280914bbbf5f12bf1e9a197d16ecbe431498
jugtuttle <jugtuttle@gmail.com>
2020-02-04 17:48:59 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,28 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const renderMovies = act => {
+    return act.movies.map(mov => {
+      return <ul>{mov}</ul>
+    })
+  }
+
+  const renderActors = () => {
+    return actors.map(act => {
+      return (
+        <div>
+          <h3>{act.name}</h3>
+          {renderMovies(act)}
+        </div>)
+    })
+  }
+
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { movies } from '../data';
 
+
+
 const Movies = () => {
+
+  const renderGenres = movie => {
+    return movie.genres.map(genre => {
+      return <ul>{genre}</ul>
+    })
+  }
+
+  const renderMovies = () => {
+    return movies.map(movie => {
+      return (
+      <div>
+        <h3>{movie.title}</h3>
+        Time: {movie.time}
+        <br></br>
+        <br></br>
+        Genres:
+        {renderGenres(movie)}
+      </div>
+      )
+    })
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {renderMovies()}
     </div>
   );
 };
